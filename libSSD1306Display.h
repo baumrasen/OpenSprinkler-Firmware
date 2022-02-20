08977f2cad5de67e24d0cc755f5f3c993c45ddb0
baumrasen <baumrasen@gmail.com>
2022-02-20 09:57:18 +0100

    missed some brackets

--- libSSD1306Display.h
+++ libSSD1306Display.h
@@ -57,7 +57,7 @@ public:
 			drawString8x16(SSD1306::OledPoint{cx, cy},
 						(char)c,
 						SSD1306::PixelStyle::Set,
-						SSD1306::OledI2C;
+						SSD1306::OledI2C);
 		}
 		cx += fontWidth;
 		// display();	// todo: not very efficient
@@ -76,7 +76,7 @@ public:
 		drawString8x16(SSD1306::OledPoint{cx, cy},
                                String(s),
                                SSD1306::PixelStyle::Set,
-                               SSD1306::OledI2C;
+                               SSD1306::OledI2C);
 
 		cx += fontWidth*nc;
 		// display();	// todo: not very efficient
